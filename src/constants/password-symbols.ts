86f48ee85f13cb81121e1bd59b61f0dcf988e485
xauroxs <xauroxs@gmail.com>
2023-10-14 10:33:09 +0300

    create symbols for passwords

---
+++ src/constants/password-symbols.ts
@@ -0,0 +1,7 @@
+export const lowercaseLetters = "qwertyuiopasdfghjklzxcvbnm";
+
+export const uppercaseLetters = "QWERTYUIOPASDFGHJKLZXCVBNM";
+
+export const digits = "0123456789";
+
+export const specialSymbols = "~!@#$%^&*()_+-=,./<>?";
